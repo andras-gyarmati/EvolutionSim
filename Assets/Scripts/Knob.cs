099604dfa6e0960fe90a0bec85e3940b7d2b2daf
Andras Gyarmati <bandieee16@gmail.com>
2019-05-06 23:25:42 +0200

    Add project

---
+++ Assets/Scripts/Knob.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class Knob
+{
+    private GameObject _gameObject;
+
+    public Knob(GameObject gameObject)
+    {
+        _gameObject = gameObject;
+    }
+
+}
